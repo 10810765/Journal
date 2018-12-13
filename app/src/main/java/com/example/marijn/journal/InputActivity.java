eab70a5e804bdc3e8c347f2b9dda5a106428b202
10810765 <10810765@users.noreply.github.com>
2018-12-13 19:15:30 +0100

    Code greatly improved

--- app/src/main/java/com/example/marijn/journal/InputActivity.java
+++ app/src/main/java/com/example/marijn/journal/InputActivity.java
@@ -13,13 +13,57 @@ import android.widget.TextView;
 
 public class InputActivity extends AppCompatActivity {
 
-    String mood;
+    private static final String SAVED_TEXT = "text_input";
+
+    private String mood;
+    private ImageView[] moodButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-        mood = "";
+
+        moodButtons = new ImageView[]{
+                findViewById(R.id.sad),
+                findViewById(R.id.worried),
+                findViewById(R.id.neutral),
+                findViewById(R.id.happy)
+        };
+
+        if (savedInstanceState == null) {
+            mood = "";
+            return;
+        } else {
+            mood = savedInstanceState.getString("mood");
+            ImageView moodImage = findViewById(R.id.pictures).findViewWithTag(mood);
+
+            final ColorMatrix grayscaleMatrix = new ColorMatrix();
+            grayscaleMatrix.setSaturation(0);
+
+            final ColorMatrixColorFilter filter = new ColorMatrixColorFilter(grayscaleMatrix);
+
+            for (int i = 0, n = moodButtons.length; i < n; i++) {
+                if (moodButtons[i].equals(moodImage)) {
+                    moodButtons[i].setColorFilter(null);
+                } else {
+                    moodButtons[i].setColorFilter(filter);
+                }
+            }
+
+            EditText title = findViewById(R.id.titleEntry);
+            EditText content = findViewById(R.id.contentEntry);
+            title.setText(savedInstanceState.getString(SAVED_TEXT));
+            content.setText(savedInstanceState.getString(SAVED_TEXT));
+        }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState); // always call super
+        outState.putString("mood", mood);
+        String title = ((EditText) findViewById(R.id.titleEntry)).getText().toString();
+        String content = ((EditText) findViewById(R.id.contentEntry)).getText().toString();
+        outState.putString(SAVED_TEXT, title);
+        outState.putString(SAVED_TEXT, content);
     }
 
     public void addEntry(View view) {
@@ -47,53 +91,20 @@ public class InputActivity extends AppCompatActivity {
     }
 
 
-    public void onItemClick(View view){
-
-        ImageView sadImage = findViewById(getResources().getIdentifier("sad", "id", getPackageName()));
-        ImageView worriedImage = findViewById(getResources().getIdentifier("worried", "id", getPackageName()));
-        ImageView neutralImage = findViewById(getResources().getIdentifier("neutral", "id", getPackageName()));
-        ImageView happyImage = findViewById(getResources().getIdentifier("happy", "id", getPackageName()));
+    public void onItemClick(View view) {
 
         final ColorMatrix grayscaleMatrix = new ColorMatrix();
         grayscaleMatrix.setSaturation(0);
 
         final ColorMatrixColorFilter filter = new ColorMatrixColorFilter(grayscaleMatrix);
 
-        switch (view.getId()) {
-            case R.id.sad:
-                sadImage.setColorFilter(null);
-                worriedImage.setColorFilter(filter);
-                neutralImage.setColorFilter(filter);
-                happyImage.setColorFilter(filter);
-
-                mood = String.valueOf(sadImage.getTag());
-
-                break;
-            case R.id.worried:
-                sadImage.setColorFilter(filter);
-                worriedImage.setColorFilter(null);
-                neutralImage.setColorFilter(filter);
-                happyImage.setColorFilter(filter);
-
-                mood = String.valueOf(worriedImage.getTag());
-                break;
-            case R.id.neutral:
-                sadImage.setColorFilter(filter);
-                worriedImage.setColorFilter(filter);
-                neutralImage.setColorFilter(null);
-                happyImage.setColorFilter(filter);
-
-                mood = String.valueOf(neutralImage.getTag());
-                break;
-            case R.id.happy:
-                sadImage.setColorFilter(filter);
-                worriedImage.setColorFilter(filter);
-                neutralImage.setColorFilter(filter);
-                happyImage.setColorFilter(null);
-
-                mood = String.valueOf(happyImage.getTag());
-                break;
+        for (int i = 0, n = moodButtons.length; i < n; i++) {
+            if (moodButtons[i].equals(view.getId())) {
+                moodButtons[i].setColorFilter(null);
+                mood = String.valueOf(moodButtons[i].getTag());
+            } else {
+                moodButtons[i].setColorFilter(filter);
+            }
         }
-
     }
 }
